391290b318e6f70272270d10ba9244b98fa3b291
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-06-28 22:32:28 +0530

    Update v3.0.java

--- src/v3.0.java
+++ src/v3.0.java
@@ -3,3 +3,17 @@ package com.company;
 import java.util.Arrays;
 import java.util.Scanner;
 import java.sql.*;
+
+class conn{
+    Connection c;
+    Statement s;
+    public conn(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql://localhost:3308/student","root","root");
+            s =c.createStatement();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
